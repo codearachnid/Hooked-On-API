67b336ba7181c484719c80773a3a20a59dabea6c
Timothy Wood <codearachnid@gmail.com>
2022-12-05 22:56:06 -0500

    boilerplate add

---
+++ hooked-on-api.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Hooked On API
+ *
+ * @package       HOOKEDONAP
+ * @author        Timothy Wood @codearachnid
+ * @license       gplv2
+ * @version       1.0.0
+ *
+ * @wordpress-plugin
+ * Plugin Name:   Hooked On API
+ * Plugin URI:    https://mydomain.com
+ * Description:   There is no UI - this is for developer to leverage triggers, actions, and filters to interface with 3rd party services. Fully configurable to white label for your own use. Please give credit where due.
+ * Version:       1.0.0
+ * Author:        Timothy Wood @codearachnid
+ * Author URI:    https://codearachnid.com
+ * Text Domain:   hooked-on-api
+ * Domain Path:   /languages
+ * License:       GPLv3
+ * License URI:   https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hooked On API. If not, see <https://www.gnu.org/licenses/gpl-3.0.html/>.
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Include your custom code here.
